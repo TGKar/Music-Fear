55ad4594675b0681204d799c59ecf515bc3d2b22
tutgadol <barkrv@gmail.com>
2020-10-01 07:33:13 +0300

    Few more checks

--- create_data_matrix.py
+++ create_data_matrix.py
@@ -3,6 +3,8 @@ import os
 import glob
 import matplotlib.pyplot as plt
 from sklearn import linear_model
+from sklearn import ensemble
+from sklearn import ensemble
 from sklearn import metrics
 
 def create_data_mat(csv_path='./set1/mp3/Soundtrack360_mp3/'):
@@ -29,11 +31,11 @@ def calc_test_error(x, y, alpha):
     mean_err = 0
     r2 = 0
     test_slicer = int(5 * len(y) / 6)
-    reps = 100
+    reps = 10
     for i in range(reps):
         inds = np.arange(len(y))
         np.random.shuffle(inds)
-        model = linear_model.Lasso(alpha=alpha)  # try 0.01
+        model = ensemble.RandomForestRegressor(min_samples_split=5)  # try 0.01
         model.fit(x[inds[:test_slicer], :], y[inds[:test_slicer]])
         y_hat = model.predict(x)
         y_hat = np.maximum(np.minimum(8*np.ones(len(y_hat)), y_hat), np.ones(len(y_hat)))
@@ -46,27 +48,40 @@ def calc_test_error(x, y, alpha):
     return mean_err, r2
 
 def calc_training_error(x, y, alpha):
-    model = linear_model.Lasso(alpha=alpha)
+    model = ensemble.RandomForestRegressor(min_samples_split=5, oob_score=True)
     # model = linear_model.LinearRegression()  # try 0.01
     model.fit(x, y)
     y_hat = model.predict(x)
     y_hat = np.maximum(np.minimum(8 * np.ones(len(y_hat)), y_hat), np.ones(len(y_hat)))
     mean_error = np.mean(np.abs(y - y_hat))
     r2 = metrics.r2_score(y, y_hat)
-    return mean_error, r2
+    return mean_error, r2, model.feature_importances_
+
+def plot_feature_importance(importance):
+    print("Most important ind " + str(np.argsort(importance)[-5:]))
+    plt.bar(np.arange(len(importance)), importance)
+    plt.title("Random Forest Variable Importance")
+    plt.xlabel("Feature")
+    plt.ylabel("Importance")
+    plt.show()
 
 
 # create_data_mat()  #Uncomment to create data matrix from individual features csvs
 features = np.loadtxt('data_mat.csv', delimiter=',')
 fear_rating = np.loadtxt('fear_ratings.csv', delimiter=',')
 normalization_const = 0.01
-
-mean_train_error, train_r2 = calc_training_error(features, fear_rating, normalization_const)
+for i in range(10):
+    mean_train_error, train_r2, feature_importance = calc_training_error(features, fear_rating,
+                                                                    normalization_const)
+    print("Most important ind " + str(np.argsort(feature_importance)[-5:]))
 print("Train mean error: " + str(mean_train_error))
 print("Train mean R squared: " + str(train_r2))
 
+plot_feature_importance(feature_importance)
 
 mean_test_error, test_r2 = calc_test_error(features, fear_rating, normalization_const)
 print("Test mean error: " + str(mean_test_error))
 print("Test mean R squared: " + str(test_r2))
 
+# feature_names =
+# feature_importance =
